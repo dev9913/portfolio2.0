bb0ad114bab4549066784072073eedb074ccf601
DEV JANGIR <11ganeshji11@gmail.com>
2026-01-20 16:06:08 +0530

    Refactor image tag update command in Groovy script
    Updated the image tag in the Kubernetes deployment YAML using environment variable.

--- vars/image_update_k8s.groovy
+++ vars/image_update_k8s.groovy
@@ -1,10 +1,9 @@
 def call(String imagename , String tag){
 	println"Update $imagename-Image Tag..."
     sh """
-	        yq e -i  '.spec.template.spec.containers[] 
-                  | select(.name == "${imagename}") 
-                  | .image |= sub(":(.*)$"; ":${tag}")' 
-                    k8s/${imagename}-deployment.yml   
-	"""
+        export IMAGE_TAG=${tag}
+        yq -i e '.spec.template.spec.containers[0].image 
+                 |= sub(":.*"; ":" + strenv(IMAGE_TAG))' k8s/${imagename}-deployment.yml
+    """
     println"Successfully change  IMAGE-tag $tag" 
 }  
