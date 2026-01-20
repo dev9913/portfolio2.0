f94d6f6c59187728d4bcbd340fa771d2b5854c22
DEV JANGIR <11ganeshji11@gmail.com>
2026-01-20 15:45:18 +0530

    Refactor GitHub credentials handling and file path

--- vars/k8s_image_push_on_git.groovy
+++ vars/k8s_image_push_on_git.groovy
@@ -1,20 +1,20 @@
 def call(String imagename, String tag,  String username) {
     println "Login to the GIT-HUB !!"
-    withCredentials([
-      string(credentialsId: 'email', variable: 'GIT_EMAIL'),
-       usernamePassword(
-       credentialsId: 'github-creds',
-       usernameVariable: 'GIT_USER',
-       passwordVariable: 'GIT_TOKEN'
-    )]) {
- 
+   withCredentials([
+        string(credentialsId: 'email', variable: 'GIT_EMAIL'),
+        usernamePassword(
+            credentialsId: 'github-creds',
+            usernameVariable: 'GIT_USER',
+            passwordVariable: 'GIT_TOKEN'
+        )
+    ]) {
  	try {
             sh """
             git config user.name "$GIT_USER"
             git config user.email "$GIT_EMAIL"
 
             git status
-            git add ${imagename}-deployment.yml
+            git add k8s/${imagename}-deployment.yml
             git commit -m "ci: update ${imagename} image to ${tag}" || echo "Nothing to commit"
 
             git push https://${GIT_USER}:${GIT_TOKEN}@github.com/${username}/portfolio2.0.git HEAD:main
@@ -26,6 +26,6 @@ def call(String imagename, String tag,  String username) {
             println "Failed to push on github ${username}/${imagename}:${tag}."
             throw e
         }
-
-    }	     
+	     
+     }
 }
