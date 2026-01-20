c0d653723aea33253ec8815c4edff9a242953365
DEV JANGIR <11ganeshji11@gmail.com>
2026-01-20 15:33:52 +0530

    Add k8s_image_push_on_git function for GitHub push

---
+++ vars/k8s_image_push_on_git.groovy
@@ -0,0 +1,31 @@
+def call(String imagename, String tag,  String username) {
+    println "Login to the GIT-HUB !!"
+    withCredentials([
+      string(credentialsId: 'email', variable: 'GIT_EMAIL'),
+       usernamePassword(
+       credentialsId: 'github-creds',
+       usernameVariable: 'GIT_USER',
+       passwordVariable: 'GIT_TOKEN'
+    )]) {
+ 
+ 	try {
+            sh """
+            git config user.name "$GIT_USER"
+            git config user.email "$GIT_EMAIL"
+
+            git status
+            git add ${imagename}-deployment.yml
+            git commit -m "ci: update ${imagename} image to ${tag}" || echo "Nothing to commit"
+
+            git push https://${GIT_USER}:${GIT_TOKEN}@github.com/${username}/portfolio2.0.git HEAD:main
+          
+	   """
+            
+            println "Successfully pushed ${username}/${imagename}:${tag} to GitHub."
+        } catch (Exception e) {
+            println "Failed to push on github ${username}/${imagename}:${tag}."
+            throw e
+        }
+
+    }	     
+}
