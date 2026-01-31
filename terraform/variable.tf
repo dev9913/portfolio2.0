b83e8f60cef6480112e0a4e6bfb7efaef4c7afbc
DEV JANGIR <11ganeshji11@gmail.com>
2026-01-31 19:23:56 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -3,18 +3,3 @@ variable "project_Namespace"{
   type = string
 }
 
-variable "cluster_name" {
-  default = "portfolio"
-  type = string
-}
-
-variable "node_image" {
-  default = "kindest/node:v1.29.2" 
-  type = string
-}
-
-variable "ingress_controller_name" {
-  default = "nginx-controller"
-  type = string
-}
-
