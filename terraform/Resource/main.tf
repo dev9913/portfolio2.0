b1871d183b72d6c4158ff825f3cf6a7ffd8a398e
DEV JANGIR <11ganeshji11@gmail.com>
2026-02-06 23:51:52 +0530

    Update main.tf

--- terraform/Resource/main.tf
+++ terraform/Resource/main.tf
@@ -1,18 +1,18 @@
 resource "null_resource" "k3s" {
   triggers = {
-    install_hash = filemd5("${path.module}/../scripts/k3s.sh")
+    install_hash = filemd5("../scripts/k3s.sh")
   }
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
-    command     = "bash ${path.module}/../scripts/k3s.sh"
+    command     = "bash ../scripts/k3s.sh"
 
   }
 
   provisioner "local-exec" {
     when        = destroy
     interpreter = ["/bin/bash", "-c"]
-    command     = "bash ${path.module}./../scripts/k3s-destroy.sh"
+    command     = "bash ../scripts/k3s-destroy.sh"
   }
 }
 
