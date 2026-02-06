cae502fbd34f41130c1cc9d9153429195ebb6367
DEV JANGIR <11ganeshji11@gmail.com>
2026-02-06 23:45:27 +0530

    Update k3s.sh

--- terraform/scripts/k3s.sh
+++ terraform/scripts/k3s.sh
@@ -10,9 +10,7 @@ sudo apt install -y curl zip unzip ca-certificates
 
 echo " Installing kubectl"
 
-KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-
-curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
