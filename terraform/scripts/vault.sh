bdbf0be3024b4fb1d7a544f9e9344e76084cded9
DEV JANGIR <11ganeshji11@gmail.com>
2026-02-06 21:26:16 +0530

    Update vault.sh

--- terraform/scripts/vault.sh
+++ terraform/scripts/vault.sh
@@ -2,20 +2,20 @@
 set -e
 
 export VAULT_ADDR="http://vault.vault.svc.cluster.local:8200"
+export VAULT_TOKEN="$VAULT_BOOTSTRAP_TOKEN"
 
 echo "Using VAULT_TOKEN for authentication"
 vault status
 
-echo "Write app/database secrets"
+echo "Writing app/database secrets"
 vault kv put secret/creds \
   user=portfolio \
   password="$APP_PASSWORD" \
   root_password="$DB_ROOT_PASSWORD"
 
-echo "Write ArgoCD notification secrets"
+echo "Writing ArgoCD notification secrets"
 vault kv put secret/argocd/notifications \
   email-username="$USER_EMAIL" \
   email-password="$USER_EMAIL_PASSWORD"
 
 echo "Done"
-
