cb77091bec70c9ba51186b7ccc966243d2ce445e
DEV JANGIR <11ganeshji11@gmail.com>
2026-02-07 15:49:02 +0530

    Update trivy_image_scan.groovy

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,21 +1,20 @@
 def call(String imagename, String tag) {
     println "Scanning Docker Image ==> dev7878/portfolio-${imagename}:${tag}"
 
-    sh(
-        script: '''
-            set -euo pipefail
-            export TRIVY_CACHE_DIR="$WORKSPACE/.trivy-cache-${imagename}"
+    sh """
+        bash -c '
+        set -euo pipefail
+        export TRIVY_CACHE_DIR="\$WORKSPACE/.trivy-cache-${imagename}"
 
-            trivy image \
-              --scanners vuln \
-              --skip-version-check \
-              --exit-code 1 \
-              --severity CRITICAL \
-              --format table \
-              dev7878/portfolio-${imagename}:${tag} | tee trivy-${imagename}-${tag}.txt
-        ''',
-        shell: '/bin/bash'
-    )
+        trivy image \
+          --scanners vuln \
+          --skip-version-check \
+          --exit-code 1 \
+          --severity CRITICAL \
+          --format table \
+          dev7878/portfolio-${imagename}:${tag} | tee trivy-${imagename}-${tag}.txt
+        '
+    """
 
     println "IMAGE SCAN Successfully dev7878/portfolio-${imagename}:${tag}"
 }
